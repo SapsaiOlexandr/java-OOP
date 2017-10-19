a01247be7fdeb5d3039ec42642704e6bc7bad11d
SapsaiOlexandr <32492501+SapsaiOlexandr@users.noreply.github.com>
2017-10-19 13:46:16 +0300

    comit

--- StudentGrup/src/com/gmail/sapsai13/Student.java
+++ StudentGrup/src/com/gmail/sapsai13/Student.java
@@ -21,6 +21,17 @@ public class Student extends Human {
 		this.course = course;
 		this.averageScore = averageScore;
 	}
+	
+	public void info () {
+		System.out.println(super.getSurname() + " "+ super.getName() );
+		System.out.println(super.getAge() + " years old");
+		System.out.println("lives at " + super.getAddress());
+		System.out.println("Phone Number " + super.getPhoneNumber());
+		System.out.println("specialization " + specialization);
+		System.out.println("course " + course);
+		System.out.println("Average Score " + averageScore);
+		
+	}
 
 	public String getSpecialization() {
 		return specialization;
@@ -49,7 +60,7 @@ public class Student extends Human {
 	@Override
 	public String toString() {
 		return "Student [specialization=" + specialization + ", course=" + course + ", averageScore=" + averageScore
-				+ "]";
+				+  super.toString() + "]";
 	}
 	
 
